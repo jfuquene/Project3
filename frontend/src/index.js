a5a4043a4d60d3f19637ce7321ddc2b9f1c4ea92
John Fuquene <jfuquene@email.wm.edu>
2021-03-08 17:13:28 -0500

    front-end

---
+++ frontend/src/index.js
@@ -0,0 +1,18 @@
+const BASE_URL = 'http://localhost:3000/shoes'
+
+
+    fetch(BASE_URL)
+    .then(res => res.json())
+    .then(shoe => shoe.forEach(renderShoe))
+
+function renderShoe(shoe){
+    console.log(shoe)
+    let allShoes = document.querySelector('main')
+
+    let specificShoe = document.createElement('h2')
+    specificShoe.innerText = shoe.brand
+
+    allShoes.append(specificShoe)
+
+
+}
