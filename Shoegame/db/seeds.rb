27e32570fb45aa42411c52fd63b6f068572e8540
Jason Herrera <baytownjason@yahoo.com>
2021-03-09 10:33:51 -0600

    pulled new image column

--- Shoegame/db/seeds.rb
+++ Shoegame/db/seeds.rb
@@ -3,7 +3,7 @@
 
 
 
-ultraboost = Shoe.create(shoes_size: 10, color:"white", price: `${$}200`, brand:"adidas", image:"https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/a03362c76f354df2a43fac210106fb50_9366/Ultraboost_4.0_DNA_Shoes_White_G55461_01_standard.jpg")
+ultraboost = Shoe.create(shoes_size: 10, color:"white", price: 200, brand:"adidas", image:"https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/a03362c76f354df2a43fac210106fb50_9366/Ultraboost_4.0_DNA_Shoes_White_G55461_01_standard.jpg")
 
 user1 = User.create(username:"Json")
 
