e2be4fc7bd2e5d9a49ff8165db19bd360b13bc51
Jason Herrera <baytownjason@yahoo.com>
2021-03-09 11:15:45 -0600

    small shoes image

--- Shoegame/db/seeds.rb
+++ Shoegame/db/seeds.rb
@@ -1,3 +1,4 @@
+Shoe.destory_all
 
 
 
@@ -7,6 +8,18 @@ ultraboost = Shoe.create(shoes_size: 10, color:"white", price: 200, brand:"adida
 
 user1 = User.create(username:"Json")
 
-json = LikedShoe.create(shoe:shoe1, user:user1)
+json = LikedShoe.create(shoe:ultraboost, user:user1)
 
+
+#////////////////nike//////////////////
+
+roshes = Shoe.create(shoes_size: 10, color:"white", price: 200, brand:"nike", image:"https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/riur8c6bz6wpakhlgwxl/roshe-one-mens-shoe-MkTmzjJv.jpg")
+
+kyrie6 = Shoe.create(shoes_size: 10, color:"white", price: 100, brand:"nike", image:"https://media.finishline.com/i/finishline/CK5869_101_P1?$Main_gray$&bg=rgb(237,237,237)")
+
+jordan1 = Shoe.create(shoes_size: 10, color:"white", price: 200, brand:"nike", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRNyIi0u8DL4OAnJ4RHJy1IDBo5cU3P_OUyA&usqp=CAU")
+
+roshes = Shoe.create(shoes_size: 10, color:"white", price: 200, brand:"nike", image:"")
+
+roshes = Shoe.create(shoes_size: 10, color:"white", price: 200, brand:"nike", image:"")
 
