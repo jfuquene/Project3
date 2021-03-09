836de96092794d1df17763d8f1a91d8f745426aa
John Fuquene <jfuquene@email.wm.edu>
2021-03-09 12:18:39 -0500

    adidas-shoes

--- Shoegame/db/seeds.rb
+++ Shoegame/db/seeds.rb
@@ -4,9 +4,14 @@
 
 
 ultraboost = Shoe.create(shoes_size: 10, color:"white", price: `${$}200`, brand:"adidas", image:"https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/a03362c76f354df2a43fac210106fb50_9366/Ultraboost_4.0_DNA_Shoes_White_G55461_01_standard.jpg")
+yeezyboost = Shoe.create(shoes_size: 10.5, color:"white", price: 200, brand:"adidas", image:"https://cdn-images.farfetch-contents.com/15/16/43/62/15164362_26068068_1000.jpg")
+stansmiths = Shoe.create(shoes_size: 9, color:"white", price: 65, brand:"adidas", image:"https://assets.adidas.com/images/w_840,h_840,f_auto,q_auto:sensitive,fl_lossy/69721f2e7c934d909168a80e00818569_9366/Stan_Smith_Shoes_White_M20324_01_standard.jpg")
+nmd_r1 = Shoe.create(shoes_size: 10.5, color:"white", price: 150, brand:"adidas", image:"https://assets.adidas.com/images/w_840,h_840,f_auto,q_auto:sensitive,fl_lossy/189b449050ef49b1aa68a8ba010163e6_9366/NMD_R1_Shoes_White_D96635_01_standard.jpg")
+samba = Shoe.create(shoes_size: 10.5, color:"white", price: 75, brand:"adidas", image:"https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/9f1ef31bb9234043abb7abb100b20338_9366/Samba_Vegan_Shoes_White_FW2427_01_standard.jpg")
+
 
 user1 = User.create(username:"Json")
 
-json = LikedShoe.create(shoe:shoe1, user:user1)
+# json = LikedShoe.create(shoe:, user:user1)
 
 
