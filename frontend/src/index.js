7084f83aeee41d3e10bc22b1615c67c1ffb89def
John Fuquene <jfuquene@email.wm.edu>
2021-03-11 21:42:38 -0500

    notsure

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,6 +10,10 @@ document.addEventListener("DOMContentLoaded", () =>{
     .then(res => res.json())
     .then(shoe => shoe.forEach(renderShoe))
 
+
+    renderSpan()
+    
+    
     const createNewShoe = document.querySelector("#new-btn")
     
     const shoeFormContainer = document.querySelector("#shoe-form-container")
@@ -28,7 +32,6 @@ document.addEventListener("DOMContentLoaded", () =>{
     //     }
     // });
 })
-
 
 function renderForm() {
     
@@ -69,21 +72,75 @@ function renderForm() {
 }
 
 
+function renderSpan() {
+
+    let div = document.createElement("div")
+
+    let adidasSpan = document.createElement("span")
+        adidasSpan.innerText = "ADIDAS"
+        adidasSpan.className = "shoe-span"
+        adidasSpan.addEventListener("click", renderAdidas)
+
+    let nikeSpan = document.createElement("span")
+        nikeSpan.innerText = "  NIKE  "
+        nikeSpan.className = "shoe-span"
+     
+    let pumaSpan = document.createElement("span")
+        pumaSpan.innerText = "  PUMA  "    
+        pumaSpan.className = "shoe-span"
+
+    let converseSpan = document.createElement("span")
+        converseSpan.innerText = "  CONVERSE  "
+        converseSpan.className = "shoe-span"
+
+    let allSpan = document.createElement("span")
+        allSpan.innerText = "  ALL  "
+        allSpan.className = "shoe-span"
+
+    let createSpan = document.createElement("span") 
+        createSpan.innerText = "  CREATE A SHOE  "
+        createSpan.className = "shoe-span"
+
+        div.append(adidasSpan, nikeSpan, pumaSpan, converseSpan, allSpan, createSpan)
+        document.getElementById('shoe-bar').append(div)
+
+}
+
+function renderAdidas(e){
+    let ul = document.querySelector("main")
+    ul.innerHTML = ""
+    
+
+    fetch(SHOE_URL)
+    .then(res => res.json())
+    .then(ad => ad.forEach(renderAdidasShoes))
+
+}
+
+function renderAdidasShoes(ad){
+// console.log(ad)
+    let ul = document.querySelector("main")
+    if (ad.brand == "adidas"){
+        let list = document.createElement("div")
+ 
+       list.innerHTML = renderShoe(ad)
+       debugger
+       ul.append(list)
+    }
+
+}
+
 function renderShoe(shoe){
 
     let shoeBar = document.getElementById("shoe-bar")
+        
 
     let shoeSpan = document.querySelector(".shoe-span")
-        shoeSpan.addEventListener('click', (e) => {
-            filteredShoes(e, shoe)
-        })
-
-    ////////////////////////////////////////////////////////
     
     let allShoes = document.querySelector('main')
         allShoes.className = 'cards'
     let shoeRow = document.createElement('div')
-        shoeRow.className = 'card'
+        // shoeRow.className = 'card'
 
     let specificShoe = document.createElement('h2')
         specificShoe.innerText = shoe.name
@@ -97,23 +154,52 @@ function renderShoe(shoe){
     shoeImg.addEventListener('click', () => {
         renderSpecificShoe(shoe)
     })
+// debugger
+    let shoeSize = document.createElement("li")
+        shoeSize.innerText = `Size: ${shoe.shoes_size}`
+
+    let shoePrice = document.createElement("li")
+        shoePrice.innerText = `Price: ${shoe.price}` 
+
+    let shoeColor = document.createElement("li")
+        shoeColor.innerText = `Color: ${shoe.color}`
+
+    let shoeSex = document.createElement("li")
+        shoeSex.innerText = 
     
 
-    shoeRow.append(specificShoe, shoeImg)
+    shoeRow.append(specificShoe, shoeImg, shoeSize, shoePrice, shoeColor)
     allShoes.appendChild(shoeRow)
 }
 
 
 
 
-function filteredShoes(shoe, e){
-    debugger
-}
+// function filteredShoes(shoe, e){
+// }
 
 
 
 
-function renderSpecificShoe(shoe){
-  
-    
-}
+// function renderSpecificShoe(shoe){
+// //   console.log(shoe)
+
+//     let div = document.querySelector('main')
+
+//     let specificShoe = document.querySelector('h2')
+//         specificShoe.innerText = shoe.name
+
+//     let shoeImg = document.querySelector("img")
+//         shoeImg.src = shoe.image 
+
+//     let shoeSize = document.createElement("li")
+//         shoeSize.innerText = shoe.size 
+
+//     let shoePrice = document.createElement("li")
+//         shoePrice.innerText = shoe.price 
+
+//     let shoeColor = document.createElement("li")
+//         shoeColor.innerText = shoe.shoeColor
+        
+//     div.append(specificShoe, shoeImg, shoeSize, shoePrice, shoeColor)
+// }
